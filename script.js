bad0bc3281055f56319846a51c9630a6e021d151
Nongmaithem Arundas Singh <nongkakrun@gmail.com>
2023-12-03 16:04:08 +0530

    fixed the display issue

--- script.js
+++ script.js
@@ -1,87 +1,13 @@
-function add(a, b) {
-    return a + b;
-}
-
-function subtract(a, b) {
-    return a - b;
-}
-
-function multiply(a, b) {
-    return a * b;
-}
-
-function divide(a, b) {
-    if(b == 0) {
-        return 'Error! Division by zero is undefined';
-    }
-    else {
-        return a/b;
-    }
-}
-
-function operate(operator, a, b) {
-    switch(operator) {
-        case '+':
-            return add(a, b);
-        case '-':
-            return subtract(a, b);
-        case '*':
-            return multiply(a, b);
-        case '/':
-            return divide(a, b);
-        default:
-            return null;
-    }
-}
-
-let firstNumber = "";
-let secondNumber = "";
-let operator = "";
-let result = "";
-
-let numButtons = document.getElementsByClassName("numButton");
-for (let i = 0; i < numButtons.length; i++)
-{
-    numButtons[i].addEventListener("click",
-    function() {
-        if(operator == "")
-        {
-            firstNumber += this.textContent;
-            updateDisplay(firstNumber);
-        }
-        else 
-        {
-            secondNumber += this.textContent;
-            updateDisplay(secondNumber);
-        }
-    }
-    )
-}
-
-let opButtons = document.getElementsByClassName("opButton");
-for(let i = 0; i < opButtons.length; i++)
-{
-    opButtons[i].addEventListener("click",
-    function() {
-        operator = this.textContent;
-    })
-}
-
-let eqButton = document.getElementsByClassName("eqButton")[0];
-eqButton.addEventListener("click", 
-function() {
-    result = operate(operator, parseFloat(firstNumber),
-    parseFloat(secondNumber));
-    updateDisplay(result);
-}
-)
-
-let clearButton = document.getElementsByClassName("clearButton")[0];
-clearButton.addEventListener("click",
-function() {
-    firstNumber = "";
-    secondNumber = "";
-    operator = "";
-    result = "";
-    updateDisplay("");
-})
+function display(val) {
+    var v = document.getElementById('display');
+    v.value += val;
+ }
+ function Result() {
+    var num1 = document.getElementById('display').value;
+    var num2 = eval(num1);
+    document.getElementById('display').value = num2;
+ }
+ function Clear() {
+    var inp = document.getElementById('display');
+    inp.value = '';
+ }
